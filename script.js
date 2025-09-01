519eae64fcc6144f1dd213ff97f45b53fd93bb40
froggicat <166259685+froggicat@users.noreply.github.com>
2025-09-01 19:36:05 +0100

    finished the main part - sketchpad works with any inputted values, (but not in a constant space yet), and cannot delete the previous sketchpad yet.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const btn = document.querySelector("button")
 const container = document.querySelector("#container");
-//contains 4 grid rows each containing four cells
+const row = document.createElement("div");
+const column = document.createElement("div");
 
-//loop 16 times, creating a cell and adding it to grid
-for (let i = 0; i < 16; i++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    container.appendChild(cell);
+//function to remove and then remake the grid when button is clicked
+function remakeGrid(width, height) {
+    //make the new sketchpad
+    for (let i = 0; i < height; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        //loop through and make 4 columns within the row div
+        for (let i = 0; i < width; i++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            column.addEventListener("mouseenter", () => {
+                column.style.backgroundColor = "black";
+            })
+        }
+    }
 }
+
+btn.addEventListener("click", () => {
+    const width = prompt("How many squares wide do you want your sketchpad?");
+    const height = prompt("How many squares high do you want your sketchpad?");
+    remakeGrid(width, height);
+})
